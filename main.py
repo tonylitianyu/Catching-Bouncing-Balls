172812a1dfb53b3333c68f82c13dab964273469b
haorantang <haorant3@illinois.edu>
2019-10-20 22:12:10 -0500

    pingpong

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import time
 import math
 from FK_calculation import FK_calculation
 import numpy as np
-
+import random
 
 
 
@@ -44,13 +44,55 @@ if clientID!=-1:
     res, endEffectorOri = vrep.simxGetObjectOrientation(clientID,posensor,-1,vrep.simx_opmode_streaming)# initialize sensor orientation
 
     #ping pong ball
-    res,ball=vrep.simxGetObjectHandle(clientID,'Sphere',vrep.simx_opmode_blocking)
-    res,balllin,ballang = vrep.simxGetObjectVelocity(clientID,ball,vrep.simx_opmode_streaming)
-
+    #initialization of two balls
+    res,ball0=vrep.simxGetObjectHandle(clientID,'Sphere#0',vrep.simx_opmode_blocking)
+    res,balllin,ballang = vrep.simxGetObjectVelocity(clientID,ball0,vrep.simx_opmode_streaming)
     time.sleep(2)
-    res,balllin,ballang = vrep.simxGetObjectVelocity(clientID,ball,vrep.simx_opmode_buffer)
+    res,balllin,ballang = vrep.simxGetObjectVelocity(clientID,ball0,vrep.simx_opmode_buffer)
+    print('Velocity 0 is:')
     print(balllin)
-    vrep.simxSetObjectFloatParameter(clientID,ball,3001,100,vrep.simx_opmode_oneshot)
+    vrep.simxSetObjectFloatParameter(clientID,ball0,3001,100,vrep.simx_opmode_buffer)
+    time.sleep(1)
+    vrep.simxSetObjectPosition(clientID,ball0,-1,[random.uniform(-0.3,0.2),random.uniform(-0.3,0.2),random.uniform(0.1,0.4)],vrep.simx_opmode_oneshot)
+    time.sleep(1)
+
+    res,ball1=vrep.simxGetObjectHandle(clientID,'Sphere#1',vrep.simx_opmode_blocking)
+    res,balllin,ballang = vrep.simxGetObjectVelocity(clientID,ball1,vrep.simx_opmode_streaming)
+    time.sleep(2)
+    res,balllin,ballang = vrep.simxGetObjectVelocity(clientID,ball1,vrep.simx_opmode_buffer)
+    print('Velocity 1 is:')
+    print(balllin)
+    vrep.simxSetObjectFloatParameter(clientID,ball1,3001,100,vrep.simx_opmode_buffer)
+    time.sleep(1)
+    vrep.simxSetObjectPosition(clientID,ball1,-1,[random.uniform(-0.2,0.3),random.uniform(-0.2,0.1),random.uniform(0.1,0.2)],vrep.simx_opmode_oneshot)
+    time.sleep(1)
+
+    res,ball2=vrep.simxGetObjectHandle(clientID,'Sphere#2',vrep.simx_opmode_blocking)
+    res,balllin,ballang = vrep.simxGetObjectVelocity(clientID,ball2,vrep.simx_opmode_streaming)
+    time.sleep(2)
+    res,balllin,ballang = vrep.simxGetObjectVelocity(clientID,ball2,vrep.simx_opmode_buffer)
+    print('Velocity 2 is:')
+    print(balllin)
+    vrep.simxSetObjectFloatParameter(clientID,ball2,3001,100,vrep.simx_opmode_buffer)
+    time.sleep(1)
+    vrep.simxSetObjectPosition(clientID,ball2,-1,[random.uniform(-0.3,0.2),random.uniform(-0.2,0.2),random.uniform(0.1,0.4)],vrep.simx_opmode_oneshot)
+    time.sleep(1)
+
+    balls = []
+    balls.append(ball0)
+    balls.append(ball1)
+    balls.append(ball2)
+    while (1):
+        for i in range(len(balls)):
+            temppos = vrep.simxGetObjectPosition(clientID,balls[i],-1,vrep.simx_opmode_oneshot)
+            print(temppos)
+            vrep.simxSetObjectPosition(clientID, balls[i], -1,
+                                       [(temppos[1][0])+random.uniform(-0.07, 0.07), (temppos[1][1])+random.uniform(-0.07, 0.07), (temppos[1][2])+random.uniform(-0.07, 0.07)],
+                                       vrep.simx_opmode_streaming)
+            time.sleep(0.1)
+
+
+
 
     time.sleep(2)
 
@@ -154,7 +196,6 @@ if clientID!=-1:
     res, endEffectorOri = vrep.simxGetObjectOrientation(clientID,posensor,-1,vrep.simx_opmode_buffer)
     print('end effector final orientation:')
     print(endEffectorOri)# initialize sensor orientation
-
 
 
 
