6a3a848b894a86b27dcaea0bcbb2c39e9ff29f14
haorantang <haorant3@illinois.edu>
2019-12-09 16:40:48 -0600

    final fix

--- InvK.py
+++ InvK.py
@@ -13,7 +13,6 @@ class InvK:
 
     def find_M(self,endEffector):
         #rotate y by -90 degree
-        #R = np.array([[0,0, -1],[0,1,0],[1,0,0]])
         R = np.array([[0,-1, 0],[1,0,0],[0,0,1]])
         p = np.array([endEffector]).transpose()
         M_temp = np.concatenate([R,p],axis=1)
@@ -28,11 +27,7 @@ class InvK:
         thetas = np.zeros(((self.S).shape[1], 1))
         V = np.ones((6, 1))
         while np.linalg.norm(V) > max_err and max_iter > 0:
-            #fk = FK_calculation(thetas,self.S)
-            #m = fk.find_M(endEffector)
-            #curr_pose = evalT(self.S, thetas,self.M)
             curr_pose = mr.FKinSpace(self.M, self.S, thetas)
-            #curr_pose = fk.find_T()
             V = inv_bracket(logm(np.dot(self.T,(inv(curr_pose)))))
             J = mr.JacobianSpace(self.S, thetas)
             pinv = inv(J.transpose().dot(J) + mu * np.identity((self.S).shape[1])).dot(J.transpose())
@@ -51,27 +46,14 @@ class InvK:
 
 """helper function"""
 def inv_bracket(m):
-    rtn = []
+    rt = []
     m = np.asarray(m)
     if (m.shape == (4, 4)):
-        rtn = np.block([[inv_bracket(m[:3, :3])],
-                        [m[:3, 3:]]])
+        rt = np.block([[inv_bracket(m[:3, :3])], [m[:3, 3:]]])
     elif (m.shape == (3, 3)):
         m = m - m.transpose()
-        rtn = np.zeros((3, 1))
-        rtn[2] = - m[0][1] / 2
-        rtn[1] = m[0][2] / 2
-        rtn[0] = - m[1][2] / 2
-    return rtn
-
-def evalT(S, theta, M):
-    ret = np.identity(4)
-    for t in toTs(S, theta):
-        ret = ret.dot(t)
-    return ret.dot(M)
-
-def toTs(S, theta):
-    return [expm(skew4(S[:,i]) * theta[i]) for i in range(S.shape[1])]
-
-def skew4(V_b):
-    return np.array([[0, -1 * V_b[2], V_b[1], V_b[3]], [V_b[2], 0, -1 * V_b[0], V_b[4]], [-1 * V_b[1], V_b[0], 0, V_b[5]], [0, 0, 0, 0]])
+        rt = np.zeros((3, 1))
+        rt[2] = - m[0][1] / 2
+        rt[1] =   m[0][2] / 2
+        rt[0] = - m[1][2] / 2
+    return rt
